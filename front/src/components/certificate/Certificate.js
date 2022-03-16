9c1cdd467026b880d815589e3fc99ef76c661534
leejooan <75022890+leejooan@users.noreply.github.com>
2022-03-16 14:15:32 +0900

    front_Certificate

--- front/src/components/certificate/Certificate.js
+++ front/src/components/certificate/Certificate.js
@@ -1,51 +1,27 @@
-import React, { useEffect, useState } from "react";
-import { Card, Button, Row, Col } from "react-bootstrap";
-import * as Api from "../../api";
-import Certificate from "./Certificate";
-import CertificateAddForm from "./CertificateAddForm";
+import React, { useState } from "react";
+import CertificateCard from "./CertificateCard";
+import CertificateEditForm from "./CertificateEditForm";
 
-function Certificates({ portfolioOwnerId, isEditable }) {
+function Certificate({ certificate, setCertificates, isEditable }) {
 
-  const [certificates, setCertificates] = useState([]);
-
-  const [isAdding, setIsAdding] = useState(false);
-
-  useEffect(() => {
-
-    Api.get("certificatelist", portfolioOwnerId).then((res) =>
-      setCertificates(res.data)
-    );
-  }, [portfolioOwnerId]);
-
+  const [isEditing, setIsEditing] = useState(false);
   return (
-    <Card>
-      <Card.Body>
-        <Card.Title>자격증</Card.Title>
-        {certificates.map((certificate) => (
-          <Certificate
-            key={certificate.id}
-            certificate={certificate}
-            setCertificates={setCertificates}
-            isEditable={isEditable}
-          />
-        ))}
-        {isEditable && (
-          <Row className="mt-3 text-center mb-4">
-            <Col sm={{ span: 20 }}>
-              <Button onClick={() => setIsAdding(true)}>+</Button>
-            </Col>
-          </Row>
-        )}
-        {isAdding && (
-          <CertificateAddForm
-            portfolioOwnerId={portfolioOwnerId}
-            setCertificates={setCertificates}
-            setIsAdding={setIsAdding}
-          />
-        )}
-      </Card.Body>
-    </Card>
+    <>
+      {isEditing ? (
+        <CertificateEditForm
+          currentCertificate={certificate}
+          setCertificates={setCertificates}
+          setIsEditing={setIsEditing}
+        />
+      ) : (
+        <CertificateCard
+          certificate={certificate}
+          isEditable={isEditable}
+          setIsEditing={setIsEditing}
+        />
+      )}
+    </>
   );
 }
 
-export default Certificates;
+export default Certificate;
