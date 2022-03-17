5b1d8e797208b5b810e747626946374da94dc4b3
Kimwater <iamkimwater@gmail.com>
2022-03-17 20:53:32 +0900

    Initial commit

---
+++ front/src/components/award/AwardEditForm.js
@@ -0,0 +1,90 @@
+import React, { useState } from "react";
+import { Button, Form, Card, Col, Row } from "react-bootstrap";
+import DatePicker from "react-datepicker";
+import * as Api from "../../api";
+
+function AwardEditForm({ award, setIsEditing, setAwards }) {
+
+  const [title, setTitle] = useState(award.title);
+  
+  const [description, setDescription] = useState(award.description);
+
+	const [authority, setAuthority] = useState(award.authority);
+
+	const [whenDate, setWhenDate] = useState(new Date(award.when_date));
+
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    
+    const res = await Api.put(`awards/${award.id}`, {
+      title,
+      description,
+			authority,
+			when_date,
+    });
+    
+    const updatedAward = res.data;
+    
+    setAwards(updatedAward);
+
+    setIsEditing(false);
+  };
+
+  return (
+    <Card className="mb-2">
+      <Card.Body>
+        <Form onSubmit={handleSubmit}>
+          <Form.Group controlId="awardEditTitle" className="mb-3">
+            <Form.Control
+              type="text"
+              placeholder="수상내역"
+              value={title}
+              onChange={(e) => setTitle(e.target.value)}
+            />
+          </Form.Group>
+
+          <Form.Group controlId="awardEditDescription">
+            <Form.Control
+              type="text"
+              placeholder="상세내역"
+              value={description}
+              onChange={(e) => setDescription(e.target.value)}
+            />
+          </Form.Group>
+
+					<Form.Group controlId="awardEditAuthority">
+            <Form.Control
+              type="text"
+              placeholder="발급기관"
+              value={authority}
+              onChange={(e) => setAuthority(e.target.value)}
+            />
+          </Form.Group>
+
+					<Form.Group as={Row} className="mt-3">
+						<Col xs="auto">
+							<DatePicker
+								selected={whenDate}
+								onChange={(date) => setWhenDate(date)}
+							/>
+						</Col>
+      		</Form.Group>
+
+          <Form.Group as={Row} className="mt-3 text-center">
+            <Col sm={{ span: 20 }}>
+              <Button variant="primary" type="submit" className="me-3">
+                확인
+              </Button>
+              <Button variant="secondary" onClick={() => setIsEditing(false)}>
+                취소
+              </Button>
+            </Col>
+          </Form.Group>
+        </Form>
+      </Card.Body>
+    </Card>
+  );
+}
+
+export default AwardEditForm;
---
+++ front/src/components/award/Awards.js
@@ -0,0 +1,48 @@
+import React, { useState, useEffect } from 'react';
+import { Card, Row, Button, Col } from "react-bootstrap";
+import AwardAddForm from './AwardAddForm';
+import Award from './Award';
+import * as Api from "../../api";
+
+function Awards({ portfolioOwnerId, isEditable }) {
+	
+	const [isAdding, setIsAdding] = useState(false);
+	
+	const [awards, setAwards] = useState(null);
+	
+	const awardList = awards.map((award) => <Award award={award} isEditable={isEditable} setAwards={setAwards} />);
+
+	useEffect(() => {
+		
+		Api.get(`awards`, portfolioOwnerId).then((res) => setAwards(res.data));
+	}, [portfolioOwnerId]);
+
+	return (
+		<>
+			<Card className="mb-2 ms-3 mr-5" style={{ width: "72rem" }}>
+				<Card.Body>
+					<Card.Title>수상내역</Card.Title>
+					{awardList}
+					{isEditable && (
+						<Col>
+							<Row className="mt-3 text-center text-info">
+								<Col sm={{ span: 20 }}>
+									<Button onClick={() => setIsAdding(true)}>+</Button>
+								</Col>
+							</Row>
+						</Col>
+					)}
+					{isAdding && (
+						<AwardAddForm
+							portfolioOwnerId={portfolioOwnerId}
+							setAwards={setAwards}
+							setIsAdding={setIsAdding}
+						/>
+					)}
+				</Card.Body>
+			</Card>
+		</>
+	);
+}
+
+export default Awards;
