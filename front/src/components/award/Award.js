c32e4ad538ebd2449eceb428f9422a60bd617136
Kimwater <iamkimwater@gmail.com>
2022-03-19 02:29:24 +0900

    convert indentation to spaces

--- front/src/components/award/Award.js
+++ front/src/components/award/Award.js
@@ -3,24 +3,24 @@ import AwardCard from './AwardCard';
 import AwardEditForm from './AwardEditForm';
 
 function Award({ award, setAwards, isEditable }) {
-	const [ isEditing, setIsEditing ] = useState(false);
-	return (
-		<>
-			{isEditing ? (
-				<AwardEditForm
-					award={award}
-					setAwards={setAwards}
-					setIsEditing={setIsEditing}
-				/>
-			) : (
-				<AwardCard
-					award={award}
-					setIsEditing={setIsEditing}
-					isEditable={isEditable}
-				/>
-			)}
-		</>
-	);
+  const [ isEditing, setIsEditing ] = useState(false);
+  return (
+    <>
+      {isEditing ? (
+        <AwardEditForm
+          award={award}
+          setAwards={setAwards}
+          setIsEditing={setIsEditing}
+        />
+      ) : (
+        <AwardCard
+          award={award}
+          setIsEditing={setIsEditing}
+          isEditable={isEditable}
+        />
+      )}
+    </>
+  );
 }
 
 export default Award;
