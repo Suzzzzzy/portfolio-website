f52e66a0b5970134f6e745f1095900c58288b4bc
KwanHoo <49335804+KwanHoo@users.noreply.github.com>
2022-03-19 13:09:02 +0900

    merge conflict - 수정
    merge conflict - 수정

--- front/src/components/Portfolio.js
+++ front/src/components/Portfolio.js
@@ -6,7 +6,6 @@ import { UserStateContext } from "../App";
 import * as Api from "../api";
 import User from "./user/User";
 import Certificates from "./certificate/Certificates";
-import Projects from "./project/Projects";
 
 function Portfolio() {
   const navigate = useNavigate();
@@ -63,19 +62,15 @@ function Portfolio() {
           />
         </Col>
         <Col>
+
           <div style={{ textAlign: "center" }}>
             학력 목록, 수상이력 목록, 프로젝트 목록, 자격증 목록 만들기
           </div>
-          <div>
-            <Projects
-              portfolioOwnerId={portfolioOwner.id}
-              isEditable={portfolioOwner.id === userState.user?.id}
-            />
-            <Certificates
-              portfolioOwnerId={portfolioOwner.id}
-              isEditable={portfolioOwner.id === userState.user?.id}
-            />
-          </div>
+          <Certificates
+            portfolioOwnerId={portfolioOwner.id}
+            isEditable={portfolioOwner.id === userState.user?.id}
+          />
+
         </Col>
       </Row>
     </Container>
