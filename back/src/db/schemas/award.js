f19650289568d3e21c354df74acfb7ac84e036cd
Suji <suji9709@gmail.com>
2022-03-19 16:38:09 +0900

    bakc-Award에서 브랜치 복붙

---
+++ back/src/db/schemas/award.js
@@ -0,0 +1,31 @@
+import { Schema, model } from "mongoose";
+
+const AwardSchema = new Schema(
+  {
+    title: {
+      type: String,
+      required: true,
+    },
+    description: {
+      type: String,
+      required: false,
+      default: "설명이 아직 없습니다. 추가해 주세요.",
+    },
+    authority: {
+      type: String, //발급기관
+      required: true,
+    },
+    when_date: {
+      type: String ,
+      required: false,
+      default: "0000.00.00"
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+const AwardModel = model("Award", AwardSchema);
+
+export { AwardModel };
